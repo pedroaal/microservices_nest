92a9e788474f974412ccf75e35a2b7d99e78cd81
PedroAltamirano <pedroaal04@gmail.com>
2023-07-30 16:52:40 -0500

    fixes

--- gateway/src/app.service.ts
+++ gateway/src/app.service.ts
@@ -5,7 +5,7 @@ import { ClientProxy } from '@nestjs/microservices';
 export class AppService {
   constructor(
     @Inject('MICROSERVICE_1') private readonly client1: ClientProxy,
-    // @Inject('MICROSERVICE_2') private readonly client2: ClientProxy,
+    @Inject('MICROSERVICE_2') private readonly client2: ClientProxy,
   ) { }
 
   getHello(): string {
@@ -23,6 +23,6 @@ export class AppService {
 
   getService2(): any {
     console.log('service_2');
-    return this.client1.send({ cmd: 'get_service_2' }, 'gateway');
+    return this.client2.send({ cmd: 'get_service_2' }, 'gateway');
   }
 }
--- service2/src/main.ts
+++ service2/src/main.ts
@@ -7,22 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      // transport: Transport.RMQ,
-      // transport: Transport.REDIS,
-      transport: Transport.KAFKA,
+      transport: Transport.RMQ,
       options: {
-        // rabitmq
-        // urls: [`amqp://${QUEUE}:5672`],
-        // queue: 'service_2_queue',
-        // queueOptions: {
-        //   durable: true,
-        // },
-        // redis
-        // host: QUEUE,
-        // port: 6379,
-        client: {
-          brokers: ['localhost:9092'],
-        }
+        urls: [QUEUE],
+        queue: 'service_2_queue',
+        queueOptions: {
+          durable: true,
+        },
       },
     },
   );
